1e8717d4bedf1943f0ec1a3fc9e860b943ad71f8
i-archanasenthil <i.archanasenthil@gmail.com>
2025-08-14 14:36:27 -0600

    App Rough Draft

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 
 st.set_page_config(page_title= "Streamlit Snowflake Demo", layout = "wide", initial_sidebar_state = "expanded")
@@ -26,6 +27,17 @@ if st.sidebar.button("Sales Table"):
     st.session_state.active_page = "Table"
 if st.sidebar.button("Dashboard"):
     st.session_state.active_page = "Dashboard"
+
+st.sidebar.header("Filters")
+if "Platform" in st.session_state.original_df.columns:
+    selected_platform = st.sidebar.selectbox("Select Publisher", options= ["All"] + sorted(st.session_state.original_df["Platform"].dropna().unique().tolist()))
+else:
+    selected_platform = "All"
+if "Genre" in st.session_state.original_df.columns:
+    selected_genre = st.sidebar.selectbox("Select Genre", options = ["All"] + sorted(st.session_state.original_df["Genre"].dropna().unique().tolist()))
+else:
+    selected_genre = "All"
+
 
 if 'show_uploader' not in st.session_state:
     st.session_state.show_uploader = False
@@ -62,4 +74,59 @@ if st.session_state.active_page == "Table":
         st.session_state.original_df = st.session_state.editable_df.copy()
         st.session_state.original_df.to_csv("vgsales.csv", index = False)
         st.success("Changes Saved")
-        
+
+#Display for the dashboard
+if st.session_state.active_page == "Dashboard":
+
+    st.header("Sales Analysis")
+    filtered_df = st.session_state.original_df.copy()
+    
+
+    if selected_genre != "All":
+        filtered_df = filtered_df[filtered_df["Genre"] == selected_genre]
+    if selected_platform != "All":
+        filtered_df = filtered_df[filtered_df["Platform"] == selected_platform]
+      
+    total_NA_sales =  filtered_df["NA_Sales"].sum()
+    total_EU_sales = filtered_df["EU_Sales"].sum()
+    total_JP_sales = filtered_df["JP_Sales"].sum()
+    total_global_sales = filtered_df["Global_Sales"].sum()
+    
+    c1, c2, c3, c4 = st.columns(4)
+    c1.metric("Total Sales to Date", f"${total_global_sales:,.2f}")
+    c2.metric("North America Sales to Date", f"${total_NA_sales:,.2f}")
+    c3.metric("European Union Sales to Date", f"${total_EU_sales:,.2f}")
+    c4.metric("Japan Sales to Date", f"${total_JP_sales:,.2f}")
+
+    col1, col2 = st.columns(2)
+    col1.subheader("Total sales per year")
+    fig = px.line(filtered_df, x="Year", y="Global_Sales")
+    fig.update_layout(yaxis_title="Sales ($)", xaxis_title= "Year")
+    col1.plotly_chart(fig, use_container_width= True)
+
+    col2.subheader("Distribution of Sales (Last 5 Years)")
+    last_year = filtered_df["Year"]
+    last5_df = filtered_df[filtered_df['Year'] >= last_year - 4]
+    melted_df = last5_df.melt(
+        id_vars='Year',
+        value_vars = ['NA_Sales','EU_Sales', 'JP_Sales','Other_Sales'],
+        var_name= 'Region',
+        value_name= 'Sales'
+    )
+
+    fig2 = px.bar(
+        melted_df,
+        x = 'Year',
+        y = 'Sales',
+        color = 'Region',
+        barmode = 'group',
+        text = 'Sales'
+    )
+
+    fig2.update_layout(
+        yaxis_title = "Total Sales ($)",
+        xaxis_title = "Year",
+        uniformtext_minsize = 8,
+        uniformtext_mode = 'hide'
+    )
+    col2.plotly_chart(fig2, use_container_width= True)
